f52ccce3e1e0b925f75306fe414dc4d1d724ebf4
syntaxmonkey2px <eopqrs@gmail.com>
2025-03-17 22:21:55 +0000

    pt5 final - vsc commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-usher.netlify.app/"
-});
- 
+  site: "https://astro-usher.netlify.app/",
+  integrations: [preact()]
+})
